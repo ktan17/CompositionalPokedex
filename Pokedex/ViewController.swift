0de8c906b512a4a2b3950294d3e4c8f4fa0ac862
Kevin Tan <ktan17@g.ucla.edu>
2022-08-28 14:49:20 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Kevin Tan on 8/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
